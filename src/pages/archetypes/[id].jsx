996708b2ea8556b06cfe502e8a3393872aafc4f2
Philosiq <mthendersonjr@gmail.com>
2025-04-27 12:48:19 -0400

    Update [id].jsx
    858

--- src/pages/archetypes/[id].jsx
+++ src/pages/archetypes/[id].jsx
@@ -855,7 +855,7 @@ export default function ArchetypeDetail() {
       ],
       color: "from-green-700 to-teal-400",
     },
-    high-priest: {
+    "high-priest": {
       id: "high-priest",
       name: "The High Priest",
       traits: ["Equity", "Authoritarian", "Conservative", "Religious", "Nationalist"],
