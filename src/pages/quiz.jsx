771c248bc59897c1f0e57b940eb6c02db32b48df
Philosiq <mthendersonjr@gmail.com>
2025-05-23 12:08:13 -0400

    Update quiz.jsx

--- src/pages/quiz.jsx
+++ src/pages/quiz.jsx
@@ -521,7 +521,7 @@ export default function QuizPage() {
                       <FaClipboardCheck className="text-3xl" />
                     </div>
                     <p className="text-sm opacity-90">
-                      96 questions • ~25 minutes
+                      92 questions • ~25 minutes
                     </p>
                   </div>
                   <div className="p-6">
