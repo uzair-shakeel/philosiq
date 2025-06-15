ff5c47cbfe73ed9b0c84f6076b09f7412da32936
uzair-shakeel <uzairshakeel00000@gmail.com>
2025-06-15 23:36:50 +0500

    ...

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,22 +1,9 @@
 import { MongoClient } from "mongodb";
 
 const MONGODB_URI =
-  process.env.MONGODB_URI ||
   "mongodb://uzair:uzair123@ac-gxnmdjp-shard-00-00.cpammnv.mongodb.net:27017,ac-gxnmdjp-shard-00-01.cpammnv.mongodb.net:27017,ac-gxnmdjp-shard-00-02.cpammnv.mongodb.net:27017/Philosiq?ssl=true&replicaSet=atlas-bfokwp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=API";
 
-const MONGODB_DB = process.env.MONGODB_DB || "Philosiq";
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
-
-if (!MONGODB_DB) {
-  throw new Error(
-    "Please define the MONGODB_DB environment variable inside .env.local"
-  );
-}
+const MONGODB_DB = "Philosiq";
 
 let cachedClient = null;
 let cachedDb = null;
