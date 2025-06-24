e17d53adec7514ae03312c3b125b6d20e93063fb
Uzair Shakeel <144268023+uzair-shakeel@users.noreply.github.com>
2025-06-24 23:38:17 +0500

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,9 +1,5 @@
 import { MongoClient } from "mongodb";
 
-const MONGODB_URI =
-  "mongodb://uzair:uzair123@ac-gxnmdjp-shard-00-00.cpammnv.mongodb.net:27017,ac-gxnmdjp-shard-00-01.cpammnv.mongodb.net:27017,ac-gxnmdjp-shard-00-02.cpammnv.mongodb.net:27017/Philosiq?ssl=true&replicaSet=atlas-bfokwp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=API";
-
-const MONGODB_DB = "Philosiq";
 
 let cachedClient = null;
 let cachedDb = null;
@@ -13,9 +9,12 @@ export async function connectToDatabase() {
     return { client: cachedClient, db: cachedDb };
   }
 
-  const client = await MongoClient.connect(MONGODB_URI, {});
+  const client = await MongoClient.connect(
+    "mongodb://uzair:uzair123@ac-gxnmdjp-shard-00-00.cpammnv.mongodb.net:27017,ac-gxnmdjp-shard-00-01.cpammnv.mongodb.net:27017,ac-gxnmdjp-shard-00-02.cpammnv.mongodb.net:27017/Philosiq?ssl=true&replicaSet=atlas-bfokwp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=API",
+    {}
+  );
 
-  const db = client.db(MONGODB_DB);
+  const db = client.db("Philosiq");
 
   cachedClient = client;
   cachedDb = db;
